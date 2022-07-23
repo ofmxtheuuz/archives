64cc8a78e3abefab6f71c6bd637e499feae34d2b
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-07-23 02:34:46 -0300

    Add files via upload

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\mxthe\source\repos\StreamWriter\file.txt";
+            string targetPath = @"C:\Users\mxthe\source\repos\StreamWriter\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (var sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                        Console.WriteLine(line);
+                    }
+                }
+
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
